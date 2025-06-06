cc28ec21c4613f2cfde7896185787250ce6533f5
Oleg <oleg.yogan@gmail.com>
2025-06-06 10:55:53 +0300

    feat(models): создание базовых моделей

--- Project/main/tasker/models.py
+++ Project/main/tasker/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    user_number = models.CharField()
+    abbreviation = models.CharField()
+
+class TaskSubjects(models.Model):
+    name = models.CharField()
+    synonym = models.CharField()
+
+class Tasks(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.PROTECT)
+    subject = models.ForeignKey(TaskSubjects, on_delete=models.PROTECT)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class TaskInternetAccounts(models.Model):
+    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
+    internet_account = models.CharField()
+
+class TaskEmails(models.Model):
+    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
+    email = models.EmailField()
+
+class TaskPhoneNumbers(models.Model):
+    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
+    phone = models.IntegerField()
+
+class TaskPersons(models.Model):
+    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
+    internet_account = models.CharField()
