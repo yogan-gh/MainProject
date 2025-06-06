f084229b8c0ed40384cc2400d8b5714d44637296
Oleg <oleg.yogan@gmail.com>
2025-06-06 11:57:34 +0300

    feat(admin): регистрация моделей в админке

--- Project/main/tasker/admin.py
+++ Project/main/tasker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(TaskSubjects)
+admin.site.register(Tasks)
+admin.site.register(TaskInternetAccounts)
+admin.site.register(TaskEmails)
+admin.site.register(TaskPhoneNumbers)
+admin.site.register(TaskPersons)
