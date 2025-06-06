3ae98b411d28ab8dc488285644c27c03ed6983e2
Oleg <oleg.yogan@gmail.com>
2025-06-06 16:42:48 +0300

    feat(models): добавление статуса заданий

--- Project/main/tasker/models.py
+++ Project/main/tasker/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 # Create your models here.
+def get_default_status():
+    return TaskStatus.objects.get_or_create(status='Зарегистрирован')[0].id
 
 class Users(models.Model):
-    user_number = models.CharField()
+    user_number = models.IntegerField()
     abbreviation = models.CharField()
     def __str__(self):
-        return self.user_number
+        return str(self.user_number)
 
 class TaskSubjects(models.Model):
     name = models.CharField()
@@ -14,9 +16,15 @@ class TaskSubjects(models.Model):
     def __str__(self):
         return self.synonym
 
+class TaskStatus(models.Model):
+    status = models.CharField()
+    def __str__(self):
+        return self.status
+
 class Tasks(models.Model):
     user = models.ForeignKey(Users, on_delete=models.PROTECT)
     subject = models.ForeignKey(TaskSubjects, on_delete=models.PROTECT)
+    status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT, default=get_default_status)
     start_date = models.DateField()
     end_date = models.DateField()
     def __str__(self):
