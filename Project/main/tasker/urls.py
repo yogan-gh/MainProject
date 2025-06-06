020382e233f3f4d58fc37dd1eb8fb993dd35989f
Oleg <oleg.yogan@gmail.com>
2025-06-06 15:59:09 +0300

    feat(views): определение маршрутизации и представления для CRUD

--- Project/main/tasker/urls.py
+++ Project/main/tasker/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<int:id>", views.details),
-    path("create/", views.details),
-    path("edit/<int:id>/", views.details),
-    path("delete/<int:id>/", views.details),
+    path("<int:id>", views.detail),
+    path("create/", views.create),
+    path("edit/<int:id>/", views.edit),
+    path("delete/<int:id>/", views.delete),
     path('stats', views.stats),
     path('settings', views.settings),
     path('feedback', views.feedback),
--- Project/main/tasker/views.py
+++ Project/main/tasker/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse
+from .models import Tasks
 
 def index(request):
-    return render(request, "index.html", {'active_tab': 'index'})
+    tasks = Tasks.objects.all()
+    return render(request,
+                  "index.html",
+                  {'active_tab': 'index', "tasks": tasks})
+
+def create(request):
+    if request.method == "POST":
+        task = Tasks()
+        task.user = request.POST.get("user")
+        task.start_date = request.POST.get("name")
+        task.end_date = request.POST.get("age")
+        task.save()
+    return HttpResponseRedirect("/")
+
+def detail(request, id):
+    task = Tasks.objects.get(id=id)
+    return render(request,
+                  "detail.html",
+                  {'active_tab': 'task-details', "task": task})
+
+def edit(request, id):
+    return HttpResponse(f"<h2>ID: {id}")
+
+def delete(request, id):
+    return HttpResponse(f"<h2>ID: {id}")
 
 def stats(request):
     return render(request, "stats.html", {'active_tab': 'stats'})
@@ -11,6 +37,3 @@ def settings(request):
 
 def feedback(request):
     return render(request, "feedback.html", {'active_tab': 'feedback'})
-
-def details(request):
-    return render(request, "task-details.html", {'active_tab': 'task-details'})
