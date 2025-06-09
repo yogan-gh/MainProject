9bdedf10d25a62351401a6d07e340a312b03353d
Oleg <oleg.yogan@gmail.com>
2025-06-09 15:04:39 +0300

    feat(db): из отношений 1:М в М:М

--- Project/main/tasker/models.py
+++ Project/main/tasker/models.py
@@ -21,35 +21,35 @@ class TaskStatus(models.Model):
     def __str__(self):
         return self.status
 
+class InternetAccounts(models.Model):
+    url = models.CharField()
+    def __str__(self):
+        return self.url
+
+class Emails(models.Model):
+    email = models.EmailField()
+    def __str__(self):
+        return self.email
+
+class PhoneNumbers(models.Model):
+    numbers = models.IntegerField()
+    def __str__(self):
+        return self.numbers
+
+class Persons(models.Model):
+    person = models.CharField()
+    def __str__(self):
+        return self.person
+
 class Tasks(models.Model):
     user = models.ForeignKey(Users, on_delete=models.PROTECT)
     subject = models.ForeignKey(TaskSubjects, on_delete=models.PROTECT)
     status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT, default=get_default_status)
     start_date = models.DateField()
     end_date = models.DateField()
+    persons = models.ManyToManyField(Persons)
+    internet_accounts = models.ManyToManyField(InternetAccounts)
+    emails = models.ManyToManyField(Emails)
+    phoneNumbers = models.ManyToManyField(PhoneNumbers)
     def __str__(self):
         return f"Task to {self.user}"
-
-class TaskInternetAccounts(models.Model):
-    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
-    internet_account = models.CharField()
-    def __str__(self):
-        return self.internet_account
-
-class TaskEmails(models.Model):
-    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
-    email = models.EmailField()
-    def __str__(self):
-        return self.email
-
-class TaskPhoneNumbers(models.Model):
-    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
-    phone = models.IntegerField()
-    def __str__(self):
-        return self.phone
-
-class TaskPersons(models.Model):
-    task = models.ForeignKey(Tasks, on_delete=models.PROTECT)
-    person = models.CharField()
-    def __str__(self):
-        return self.person
