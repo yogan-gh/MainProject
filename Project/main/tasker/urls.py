3172a58d32222be664dc93f3bf0cdf9dbb940f3b
Oleg <oleg.yogan@gmail.com>
2025-06-12 10:52:38 +0300

    refactor(urls): переписал редиректы

--- Project/main/tasker/urls.py
+++ Project/main/tasker/urls.py
@@ -1,38 +1,42 @@
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from . import views
-from . import viewsSettings
+from .myviews import settings, tasks
 
 settings_patterns = [
     # Users CRUD
-    path('users/', viewsSettings.users_view, name='users_view'),
-    path('add_user/', viewsSettings.add_user, name='add_user'),
-    path('edit_user/<int:user_id>/', viewsSettings.edit_user, name='edit_user'),
-    path('delete_user/<int:user_id>/', viewsSettings.delete_user, name='delete_user'),
+    path('users/', settings.users_view, name='users_view'),
+    path('add_user/', settings.add_user, name='add_user'),
+    path('edit_user/<int:user_id>/', settings.edit_user, name='edit_user'),
+    path('delete_user/<int:user_id>/', settings.delete_user, name='delete_user'),
 
     # TaskSubjects CRUD
-    path('subjects/', viewsSettings.subjects_view, name='subjects_view'),
-    path('add_subject/', viewsSettings.add_subject, name='add_subject'),
-    path('edit_subject/<int:subject_id>/', viewsSettings.edit_subject, name='edit_subject'),
-    path('delete_subject/<int:subject_id>/', viewsSettings.delete_subject, name='delete_subject'),
+    path('subjects/', settings.subjects_view, name='subjects_view'),
+    path('add_subject/', settings.add_subject, name='add_subject'),
+    path('edit_subject/<int:subject_id>/', settings.edit_subject, name='edit_subject'),
+    path('delete_subject/<int:subject_id>/', settings.delete_subject, name='delete_subject'),
 
     # TaskStatus CRUD
-    path('statuses/', viewsSettings.statuses_view, name='statuses_view'),
-    path('add_status/', viewsSettings.add_status, name='add_status'),
-    path('edit_status/<int:status_id>/', viewsSettings.edit_status, name='edit_status'),
-    path('delete_status/<int:status_id>/', viewsSettings.delete_status, name='delete_status'),
+    path('statuses/', settings.statuses_view, name='statuses_view'),
+    path('add_status/', settings.add_status, name='add_status'),
+    path('edit_status/<int:status_id>/', settings.edit_status, name='edit_status'),
+    path('delete_status/<int:status_id>/', settings.delete_status, name='delete_status'),
 
-    # Главная страница со всеми данными
-    path('', viewsSettings.all_data_view, name='all_data'),
+    path('', settings.all_data_view, name='all_data'),
 ]
 
+tasks_patterns = [
+    path("", tasks.index),
+    path("add/", tasks.create),
+    path("<int:id>/", tasks.detail),
+    path('<int:id>/data/', tasks.adddata),
+    path("<int:id>/edit/", tasks.edit),
+    path("<int:id>/delete/", tasks.delete),
+]
 urlpatterns = [
-    path("", views.index),
-    path("<int:id>", views.detail),
-    path("create/", views.create),
-    path('create/<int:id>/adddata/', views.adddata),
-    path("edit/<int:id>/", views.edit),
-    path("delete/<int:id>/", views.delete),
-    path('stats', views.stats),
+    path("", RedirectView.as_view(url='task/', permanent=True)),
+    path('task/', include(tasks_patterns)),
+    path('stats/', views.stats),
     path('settings/', include(settings_patterns)),
-    path('feedback', views.feedback),
+    path('feedback/', views.feedback),
 ]
