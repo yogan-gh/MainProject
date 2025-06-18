1147aba02d9e11181f8701fbb2278f866aa1de33
Oleg <oleg.yogan@gmail.com>
2025-06-18 17:23:06 +0300

    feat(sec): проверка на пользователя

--- Project/main/tasker/myviews/decorators.py
+++ Project/main/tasker/myviews/decorators.py
@@ -4,3 +4,8 @@ def in_group(*group_names):
     def check(user):
         return user.groups.filter(name__in=group_names).exists()
     return user_passes_test(check)
+
+def is_user(user_check):
+    def check(user):
+        return user == user_check
+    return user_passes_test(check)
