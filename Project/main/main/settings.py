b1905f03005bab6bcb406607118b08e1da8f31dd
Oleg <oleg.yogan@gmail.com>
2025-06-24 10:03:43 +0300

    feat(db): миграция на postgresql

--- Project/main/main/settings.py
+++ Project/main/main/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskerdb',
+        'USER': 'dbuser',
+        'PASSWORD': 'q11223344',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
